3500472889563c92ec2a8a0ec3152d3636765342
İHLASMERT <ihlas1mert@gmail.com>
2022-12-21 23:35:49 +0300

    addded full js codes and completed project

--- main.js
+++ main.js
@@ -6,3 +6,18 @@ let btn = document.getElementById("btn")
 
 const url = "https://api.quotable.io/random"
 
+let getQuote = ()=>{
+    fetch(url)
+    .then(data=>data.json())
+    .then((item)=>{
+
+       /*  console.log(item.content);
+        console.log(item.author); */
+        quote.innerHTML = item.content;
+        author.innerHTML = item.author
+    })
+
+}
+
+window.addEventListener("load", getQuote)
+btn.addEventListener("click", getQuote)
